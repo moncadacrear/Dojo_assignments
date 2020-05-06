65b22f70bee15123088fd91986d13ee632f75c72
Ivan Moncada <ivanmoncada@Ivans-MacBook-Pro.local>
2020-05-06 12:49:34 -0600

    third commit

--- mern/react/propitup/src/App.js
+++ mern/react/propitup/src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 // import Mycomponent from './components/Mycomponent';
 import PersonCard from './components/PersonCard';
-
 function App() {
   return (
+    
     <div className="App">
-        <PersonCard firstName ={'Jane'} lastName={'Doe'} age={'45'} hairColor= {'black'}/>
-        <PersonCard firstName ={'John'} lastName={'Smith'} age={'88'} hairColor= {'Brown'}/> 
-        <PersonCard firstName ={' Maria'} lastName={'Smith'} age={'62'} hairColor= {'Brown'}/> 
+        <PersonCard firstName ={'Jane'} lastName={'Doe'} age={45} hairColor= {'black'}/>
+        <PersonCard firstName ={'John'} lastName={'Smith'} age={88} hairColor= {'Brown'}/> 
+        <PersonCard firstName ={' Maria'} lastName={'Smith'} age={62} hairColor= {'Brown'}/>
+        
     </div>
+
   );
 }
 
--- mern/react/propitup/src/components/PersonCard.js
+++ mern/react/propitup/src/components/PersonCard.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            addonclick:0
+        };
+    }
+    handleClick = () => {
+        this.setState({addonclick:this.state.addonclick + 1});
+    }
+
     render(){
-        const{ firstName, lastName , age , hairColor} = this.props;
+        let { firstName, lastName , age , hairColor } = this.props;
         return(
             <div>
                 <h1>{ lastName }, { firstName}  </h1>
-                <h3>Age:{age} </h3>
+                <h3>Age:{this.state.addonclick +age } </h3>
                 <h3>hair Color:{hairColor}</h3>
+        <button onClick={ this.handleClick}> Birthday {firstName} {lastName}</button>
             </div>
 
         )
+        
     }
 }
 
